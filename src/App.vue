420dc16ddeeb4f32ce1690db18cc7ffd7b13de52
ValeriaPostylyakova <postylyakova.valeria4020@mail.ru>
2024-06-17 14:44:04 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="mb-5">
+    <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem odit sed totam voluptate iusto. Ab odio explicabo tempora recusandae temporibus mollitia sunt, quo facere quasi rem, impedit, blanditiis distinctio quaerat.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
